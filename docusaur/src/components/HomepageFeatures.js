896a56efd6304da4ea598e518f0cdd6b8eb82719
Peter <pete@thecarnetwork.tv>
2021-11-21 16:35:08 -0700

    Update HomepageFeatures.js

--- docusaur/src/components/HomepageFeatures.js
+++ docusaur/src/components/HomepageFeatures.js
@@ -2,21 +2,22 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 import Favicon from '@site/static/img/favicon.ico';
-import ImageTokenMobile from '@site/static/img/image-token-mobile.png';
-import CrossChainMobile from '@site/static/img/crosschain-mobile.png';
+import mainSigma from '@site/static/img/docs-protocoloverviewB.png';
+import ImageTokenMobile from '@site/static/img/docs-systemsoverview.png';
+import CrossChainMobile from '@site/static/img/docs-API.png';
 
 
 const FeatureList = [
   {
     title: 'Protocol Overview',
-    image: Favicon,
+    image: mainSigma,
     link: 'docs/Protocol Overview/Protocol Overview',
     description: (
       <>
       A Decentralized Exchange Focused on creating
       long term incentives through the use of a time staking mechanism
       that penalizes users for premature withdrawls and rewarding users with
-      Platform Rewards in the form of NFTs
+      Platform Rewards in the form of NFTs.
       </>
     ),
   },
@@ -28,7 +29,7 @@ const FeatureList = [
       <>
       Sigmadex utilizes the EIP-2535 Diamond architecture to bring the native token SDEX,
       A multi-token farm, an autocompounding native token pool, and a Governance structure.
-      Swap/Pool systems coming Soon!
+      Swap/Pool systems are coming soon.
       </>
     ),
   },
@@ -38,7 +39,7 @@ const FeatureList = [
     link: 'docs/API Specification/Diamond',
     description: (
       <>
-      A description of the various sigmadex smart contract funtions
+      A description and overview of the various Sigmadex Smart Contract functions, enabling on-chain interaction by third party developers and other decentralized applications.
       </>
     ),
   },
