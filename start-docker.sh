d0c938a1846eee03ef2bdff61a3c815067baaf96
Nashruddin Amin <nashruddin.amin@gmail.com>
2021-12-10 13:29:44 +0700

    Moved IS_DOCKER to docker-compose.yml

--- start-docker.sh
+++ start-docker.sh
@@ -4,15 +4,6 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   export UID
 fi
 
-IS_DOCKER=$(grep IS_DOCKER ./hardhat/.env | cut -d '=' -f 2-)
-
-if [ $IS_DOCKER == "false" ]
-then
-  echo "Please set the IS_DOCKER env variable in ./hardhat to true"
-  exit 1
-fi
-
-
 SUBGRAPH_ENV=${pwd}/subgraph/.env
 
 if [ -f $SUBGRAPH_ENV ]
