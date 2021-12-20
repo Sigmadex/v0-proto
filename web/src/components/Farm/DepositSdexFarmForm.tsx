0064f197cdc285b6d516f1127421a41f1acc04d2
Joe-mcgee <thulsmans.2994@me.com>
2021-12-20 09:31:28 -0800

    rejig front end

--- web/src/components/Farm/DepositSdexFarmForm.tsx
+++ web/src/components/Farm/DepositSdexFarmForm.tsx
@@ -29,7 +29,7 @@ const DepositSdexFarmForm: FC<DepositFarmFormProps> = ({usersValidNFTs, farmid})
 
   // Form 
   const [amountA, setAmountA] = useState(0)
-  const [blocksStake, setBlocksStake] = useState(0)
+  const [timeStake, setTimeStake] = useState(0)
   const [nftAddr, setNFTAddr] = useState(Static.addresses.ZERO)
   const [nftid, setNFTid] = useState(0)
   
@@ -101,7 +101,7 @@ const DepositSdexFarmForm: FC<DepositFarmFormProps> = ({usersValidNFTs, farmid})
   const {onDeposit} = useDepositFarm(
     0, // pull this later from url params
     [amountA],
-    blocksStake,
+    timeStake,
     nftAddr, // no nft for now
     nftid
   )
@@ -166,8 +166,8 @@ const DepositSdexFarmForm: FC<DepositFarmFormProps> = ({usersValidNFTs, farmid})
           <div className="card-body">
             <div className="row">
               <div className="col-8">
-                Blocks to Stake:
-                <input type="text" name="from" className="form-control" placeholder="0.0" value={blocksStake} onChange={e => setBlocksStake(e.target.value)}/>
+                Time to Stake:
+                <input type="text" name="from" className="form-control" placeholder="0.0" value={timeStake} onChange={e => setTimeStake(e.target.value)}/>
               </div>
               <div className="col-4 text-end">
                 <br />
